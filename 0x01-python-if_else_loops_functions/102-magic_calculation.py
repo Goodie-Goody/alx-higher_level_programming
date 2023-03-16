373ed8e196095b371da9f083dee9eef10027b8cc
Kalu Goodness <goodkc12@gmail.com>
2023-03-16 03:41:00 -0700

    Trial 13

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    # Compare a and b
     if a < b:
-        # Compare c and b
         if c > b:
-            # Return c if c > b
             return c
         else:
-            # Return the sum of a and b if c <= b
             return a + b
     else:
-        # Return the result of a * b - c if a >= b
         return a * b - c
+
