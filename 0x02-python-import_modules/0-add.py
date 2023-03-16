e06f9b2337877df2657f89966d0649442566dcfd
Kalu Goodness <goodkc12@gmail.com>
2023-03-16 06:47:53 -0700

    Let's see

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+# importing the function from add_0.py file
 from add_0 import add
 
+# assigning values to variables a and b
 a = 1
 b = 2
 
-print("{0} + {1} = {2}".format(a, b, add(a, b)))
+# calling the add function with arguments a and b, and assigning the result to variable result
+result = add(a, b)
+
+# printing the result with string format
+print("{} + {} = {}".format(a, b, result))
+
